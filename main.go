7b2f4375523d9da1de627f344350f51d5eaf4ea2
liyiting <liyiting@lizhi.fm>
2023-11-06 20:00:19 +0800

    监听不同端口

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
 	http.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
 		writer.Write([]byte("hello"))
 	})
-	http.ListenAndServe(":9999", nil)
+	fmt.Println("listen port: " + os.Getenv("PORT"))
+	http.ListenAndServe(strings.Join([]string{"", os.Getenv("PORT")}, ":"), nil)
 }
