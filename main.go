c48c0a1230c6d5a6013df85a318fb93bdd18e7e5
liyiting <liyiting@lizhi.fm>
2023-11-09 18:04:52 +0800

    查看当前cpu

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"runtime"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	http.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte("hello"))
+		writer.Write([]byte(strconv.Itoa(runtime.NumCPU())))
 	})
 	fmt.Println("listen port: " + os.Getenv("PORT"))
 	http.ListenAndServe(strings.Join([]string{"", os.Getenv("PORT")}, ":"), nil)
