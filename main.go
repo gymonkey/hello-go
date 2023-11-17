8a67e3fbec624975f11aef0000a66fc3e08bab9d
liyiting <liyiting@lizhi.fm>
2023-11-17 14:10:39 +0800

    获取本地地址优化

--- main.go
+++ main.go
@@ -12,42 +12,20 @@ import (
 
 func main() {
 	http.HandleFunc("/localip", func(writer http.ResponseWriter, request *http.Request) {
-		ifaces, err := net.Interfaces()
+		ip, err := getLocalIp()
 		if err != nil {
 			writer.WriteHeader(http.StatusOK)
 			writer.Write([]byte(err.Error()))
 			return
 		}
-		// handle err
-		ips := make([]string, 0)
-		for _, i := range ifaces {
-			addrs, err := i.Addrs()
-			if err != nil {
-				writer.WriteHeader(http.StatusOK)
-				writer.Write([]byte(err.Error()))
-				return
-			}
-			// handle err
-			for _, addr := range addrs {
-				var ip net.IP
-				switch v := addr.(type) {
-				case *net.IPNet:
-					ip = v.IP
-				case *net.IPAddr:
-					ip = v.IP
-				}
-				if ip.To4() != nil {
-					ips = append(ips, ip.String())
-				}
-				// process IP address
-			}
-		}
 		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte(strings.Join(ips, ",")))
+		writer.Write([]byte(ip))
 		return
 	})
 	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
-		pinger, err := ping.NewPinger("www.google.com")
+		request.ParseForm()
+		addr := request.Form.Get("addr")
+		pinger, err := ping.NewPinger(addr)
 		if err != nil {
 			writer.WriteHeader(http.StatusOK)
 			writer.Write([]byte(err.Error()))
@@ -68,4 +46,34 @@ func main() {
 	//os.Setenv("PORT", "18080")
 	fmt.Println("listen port: " + os.Getenv("PORT"))
 	http.ListenAndServe(strings.Join([]string{"", os.Getenv("PORT")}, ":"), nil)
+}
+
+func getLocalIp() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	// handle err
+	ips := make([]string, 0)
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
+		}
+		// handle err
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip.To4() != nil {
+				ips = append(ips, ip.String())
+			}
+			// process IP address
+		}
+	}
+	return ips[0], nil
 }
