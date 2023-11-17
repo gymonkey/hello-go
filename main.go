754ae989fea61ec9969e84d066eab55242a6c154
liyiting <liyiting@lizhi.fm>
2023-11-17 12:05:32 +0800

    检查本地ip

--- main.go
+++ main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
-	"runtime"
-	"strconv"
 	"strings"
 )
 
 func main() {
-	http.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte(strconv.Itoa(runtime.NumCPU())))
+	http.HandleFunc("/localip", func(writer http.ResponseWriter, request *http.Request) {
+		ifaces, err := net.Interfaces()
+		if err != nil {
+			writer.WriteHeader(http.StatusOK)
+			writer.Write([]byte(err.Error()))
+			return
+		}
+		// handle err
+		for _, i := range ifaces {
+			addrs, err := i.Addrs()
+			if err != nil {
+				writer.WriteHeader(http.StatusOK)
+				writer.Write([]byte(err.Error()))
+				return
+			}
+			ips := make([]string, 0)
+			// handle err
+			for _, addr := range addrs {
+				var ip net.IP
+				switch v := addr.(type) {
+				case *net.IPNet:
+					ip = v.IP
+				case *net.IPAddr:
+					ip = v.IP
+				}
+				ips = append(ips, ip.String())
+				// process IP address
+			}
+			writer.WriteHeader(http.StatusOK)
+			writer.Write([]byte(strings.Join(ips, ",")))
+			return
+		}
 	})
 	fmt.Println("listen port: " + os.Getenv("PORT"))
 	http.ListenAndServe(strings.Join([]string{"", os.Getenv("PORT")}, ":"), nil)
