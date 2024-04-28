39824e5b185b6ccf3a2dc581db8d7f34f1988ad5
liyiting <liyiting@lizhi.fm>
2024-04-28 11:26:09 +0800

    模拟日志输出

--- main.go
+++ main.go
@@ -2,88 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-ping/ping"
-	"net"
-	"net/http"
-	"os"
-	"strings"
 	"time"
 )
 
 func main() {
-	http.HandleFunc("/localip", func(writer http.ResponseWriter, request *http.Request) {
-		ip, err := getLocalIp()
-		if err != nil {
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(err.Error()))
-			return
-		}
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte(ip))
-		return
-	})
-	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
-		request.ParseForm()
-		addr := request.Form.Get("addr")
-		pinger, err := ping.NewPinger(addr)
-		if err != nil {
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(err.Error()))
-			return
-		}
-		pinger.Count = 3
-		pinger.Timeout = 1 * time.Minute
-		err = pinger.Run()
-		if err != nil {
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(err.Error()))
-			return
-		}
-		stats := pinger.Statistics()
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte(fmt.Sprintf("%s %s %s %f\n", stats.MaxRtt, stats.MinRtt, stats.AvgRtt, stats.PacketLoss)))
-	})
-	http.HandleFunc("/connect", func(writer http.ResponseWriter, request *http.Request) {
-		request.ParseForm()
-		addr := request.Form.Get("addr")
-		_, err := net.DialTimeout("tcp", addr, 5*time.Second)
-		if err != nil {
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(err.Error()))
-		}
-		writer.WriteHeader(http.StatusOK)
-	})
-	//os.Setenv("PORT", "18080")
-	fmt.Println("listen port: " + os.Getenv("PORT"))
-	http.ListenAndServe(strings.Join([]string{"", os.Getenv("PORT")}, ":"), nil)
-}
-
-func getLocalIp() (string, error) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		return "", err
+	for {
+		fmt.Println(time.Now().Format(time.RFC3339))
+		time.Sleep(10 * time.Second)
 	}
-	// handle err
-	ips := make([]string, 0)
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			return "", err
-		}
-		// handle err
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip.To4() != nil {
-				ips = append(ips, ip.String())
-			}
-			// process IP address
-		}
-	}
-	return ips[1], nil
 }
