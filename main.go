dea3156ccdd3c3d19c009ef49807ea79e4006a31
liyiting <liyiting@lizhi.fm>
2024-04-28 16:56:54 +0800

    增加网络入口

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+	})
+	http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("inner"))
+	})
+	http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("outter"))
+	})
+	go http.ListenAndServe(":8080", nil)
 	for {
 		fmt.Println(time.Now().Format(time.RFC3339))
 		time.Sleep(10 * time.Second)
