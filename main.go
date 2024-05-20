7c1d274008e8371ca5a371e8041829486031b3a9
liyiting <liyiting@lizhi.fm>
2024-05-20 18:00:42 +0800

    尝试访问内网地址

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
@@ -12,9 +13,22 @@ func main() {
 		writer.WriteHeader(http.StatusOK)
 	})
 	http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.RemoteAddr)
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte("inner"))
+		resp, err := http.Get("http://10.112.7.10:8000/abc")
+		if err != nil {
+			fmt.Println(err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		data, _ := io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println(string(data))
+			writer.WriteHeader(resp.StatusCode)
+			return
+		} else {
+			writer.WriteHeader(http.StatusOK)
+			writer.Write(data)
+		}
 	})
 	http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println(request.RemoteAddr)
