c244c2bb2d2a1ce39afff5be1ba366e71e214e99
liyiting <liyiting@lizhi.fm>
2024-05-24 10:46:14 +0800

    定时输出日志

--- main.go
+++ main.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 )
 
 func main() {
-	http.DefaultClient.Timeout = 1 * time.Second
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.RemoteAddr)
-		writer.WriteHeader(http.StatusOK)
-	})
-	http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
-		//log.Println("recv req")
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte("inner"))
-	})
-	http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.RemoteAddr)
-		writer.WriteHeader(http.StatusOK)
-		writer.Write([]byte("outter"))
-	})
-	http.ListenAndServe(":8080", nil)
+	//http.DefaultClient.Timeout = 1 * time.Second
+	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Println(request.RemoteAddr)
+	//	writer.WriteHeader(http.StatusOK)
+	//})
+	//http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
+	//	//log.Println("recv req")
+	//	writer.WriteHeader(http.StatusOK)
+	//	writer.Write([]byte("inner"))
+	//})
+	//http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Println(request.RemoteAddr)
+	//	writer.WriteHeader(http.StatusOK)
+	//	writer.Write([]byte("outter"))
+	//})
+	//http.ListenAndServe(":8080", nil)
+	for {
+		fmt.Println(time.Now())
+		time.Sleep(10 * time.Second)
+	}
 }
