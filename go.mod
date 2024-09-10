d9ae9122f0ad61c34dc0af60c228f22e802e991a
liyiting <liyiting@lizhi.fm>
2024-09-10 17:50:19 +0800

    作为http代理部署

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.20
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/elazarl/goproxy v0.0.0-20240909085733-6741dbfc16a1 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.53.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,46 +1,51 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
+	"github.com/elazarl/goproxy"
+	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
-	http.DefaultClient.Timeout = 1 * time.Second
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(request.RemoteAddr)
-		writer.Write([]byte("hello world"))
-		writer.WriteHeader(http.StatusOK)
-	})
-	http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
-		ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
-		req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://ip.sb/", nil)
-		req.Header.Set("User-Agent", "curl/8.7.1")
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(err.Error()))
-		} else {
-			defer resp.Body.Close()
-			writer.WriteHeader(http.StatusOK)
-			writer.Write([]byte(resp.Status))
-			data, _ := io.ReadAll(resp.Body)
-			writer.Write(data)
-		}
-	})
-	http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
-		//fmt.Println(request.RemoteAddr)
-		writer.WriteHeader(http.StatusOK)
-		data, _ := io.ReadAll(request.Body)
-		fmt.Println(request.Header.Get("Authorization"))
-		fmt.Println(string(data))
-	})
-	http.ListenAndServe(":8080", nil)
+	//http.DefaultClient.Timeout = 1 * time.Second
+	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Println(request.RemoteAddr)
+	//	writer.Write([]byte("hello world"))
+	//	writer.WriteHeader(http.StatusOK)
+	//})
+	//http.HandleFunc("/inner", func(writer http.ResponseWriter, request *http.Request) {
+	//	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+	//	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://ip.sb/", nil)
+	//	req.Header.Set("User-Agent", "curl/8.7.1")
+	//	resp, err := http.DefaultClient.Do(req)
+	//	if err != nil {
+	//		writer.WriteHeader(http.StatusOK)
+	//		writer.Write([]byte(err.Error()))
+	//	} else {
+	//		defer resp.Body.Close()
+	//		writer.WriteHeader(http.StatusOK)
+	//		writer.Write([]byte(resp.Status))
+	//		data, _ := io.ReadAll(resp.Body)
+	//		writer.Write(data)
+	//	}
+	//})
+	//http.HandleFunc("/outter", func(writer http.ResponseWriter, request *http.Request) {
+	//	//fmt.Println(request.RemoteAddr)
+	//	writer.WriteHeader(http.StatusOK)
+	//	data, _ := io.ReadAll(request.Body)
+	//	fmt.Println(request.Header.Get("Authorization"))
+	//	fmt.Println(string(data))
+	//})
+	//http.ListenAndServe(":8080", nil)
 	//for {
 	//	fmt.Println("6" + strconv.FormatInt(time.Now().Unix(), 10))
 	//	time.Sleep(10 * time.Second)
 	//}
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	proxy.NonproxyHandler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("always return ok"))
+	})
+	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
